83341018d1e1d08331d958cfad19193c4f1c4d10
James Wordie <james.wordie@yahoo.com>
2021-03-20 12:52:01 +0000

    simple form for bidding entry

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -1,2 +1,26 @@
 class BiddingsController < ApplicationController
+  def index
+    @biddings = Bidding.all
+  end
+
+  def new
+    @bidding = Bidding.new
+  end
+
+  def create
+    @bidding = Bidding.new(bidding_params)
+    @listing = Listing.find(params[:listing_id])
+    @bidding.user = current_user
+    if @bidding.save!
+      redirect_to listings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    raise
+  end
 end
